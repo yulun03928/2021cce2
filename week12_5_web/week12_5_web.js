b9aa990fcc2215aeb3788f78c8f9717f2462c6fa
LI-YU-LUN <91250040+yulun03928@users.noreply.github.com>
2022-05-20 09:36:28 +0800

    Update week12_5_web.js

--- week12_5_web/week12_5_web.js
+++ week12_5_web/week12_5_web.js
@@ -13,16 +13,21 @@ function draw() {
     if (choice == 0)
         rect(50, 50, 150, 2);
     if (choice == 1)
-        rect(50, 50, 150, 2);
+        rect(50, 100, 150, 2);
     if (choice == 2)
-        rect(50, 50, 150, 2);
+        rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
     text("Choice 1", 50, 50);
     text("Choice 2", 50, 100);
     text("Choice 3", 50, 150);
+    text("Choice 4", 50, 200);
+    text("Choice 5", 50, 250);
+    
 }
 
 function mousePressed() {
-    choice = int(random(3));
+    choice = int(random(5));
     // choice = ( choice + 1) % 3 ;
 }
 
